2cc9eef98d23c51ae7f24e6045d4dec548ca9c64
UndineA <undinearaja@gmail.com>
2025-06-30 21:59:04 +0300

    Test No.3, fix: added attr_accessor and initialize to VendingMachine class. Tests No.5, 6, 7, 8 also passed

--- app/vending_machine.rb
+++ app/vending_machine.rb
@@ -4,6 +4,15 @@ require_relative 'transaction_processor'
 require_relative 'display_manager'
 
 class VendingMachine
+  attr_accessor :coin_manager, :product_catalog, :transaction_processor, :display_manager
+
+  def initialize
+    @coin_manager = CoinManager.new
+    @product_catalog = ProductCatalog.new
+    @transaction_processor = TransactionProcessor.new
+    @display_manager = DisplayManager.new(@transaction_processor)
+  end
+
   def insert(amount)
     @coin_manager.add_coins(amount)
     balance
